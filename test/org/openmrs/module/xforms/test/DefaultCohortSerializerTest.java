2e7a281a09fa48ccdce6664638432d2c1c2ae0e1
Daniel Kayiwa <dkayiwa@openmrs.org>
2011-04-28 21:46:09 +0000

    xforms module: Removing un used classes and files, including the latest purcforms form designer widget, and some cleaning up.

--- test/org/openmrs/module/xforms/test/DefaultCohortSerializerTest.java
+++ test/org/openmrs/module/xforms/test/DefaultCohortSerializerTest.java
@@ -1,7 +1,11 @@
 package org.openmrs.module.xforms.test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+
+import junit.framework.Assert;
 
 import org.openmrs.module.xforms.download.PatientDownloadManager;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
@@ -23,20 +27,20 @@ public class DefaultCohortSerializerTest extends BaseModuleContextSensitiveTest{
         DataOutputStream  dos = new DataOutputStream(baos);
         PatientDownloadManager.downloadCohorts(dos,null);
         
-        /*DataInputStream dis = new DataInputStream(new ByteArrayInputStream(baos.toByteArray()));
+        DataInputStream dis = new DataInputStream(new ByteArrayInputStream(baos.toByteArray()));
         byte size = dis.readByte();
-        assertTrue("There should be two cohorts",size == 2);
+        Assert.assertTrue("There should be two cohorts", size == 2);
         
         int id = dis.readInt();
-        assertTrue("The first cohort_id should be 1",id == 1);
+        Assert.assertTrue("The first cohort_id should be 1", id == 1);
         
         String name = dis.readUTF();
-        assertTrue("The first cohort name should be: The First Unit Test Cohort",name.equals("The First Unit Test Cohort"));
+        Assert.assertTrue("The first cohort name should be: The First Unit Test Cohort", name.equals("The First Unit Test Cohort"));
         
         id = dis.readInt();
-        assertTrue("The second cohort_id should be 2",id == 2);
+        Assert.assertTrue("The second cohort_id should be 2", id == 2);
         
         name = dis.readUTF();
-        assertTrue("The second cohort name should be: The Second Unit Test Cohort",name.equals("The Second Unit Test Cohort"));*/
+        Assert.assertTrue("The second cohort name should be: The Second Unit Test Cohort", name.equals("The Second Unit Test Cohort"));
     }
 }
