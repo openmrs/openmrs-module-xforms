917700d2f6777f1c8b638e15f7984b7748ad9d47
wluyima <wyclif@openmrs.org>
2012-07-09 09:32:55 -0400

    Editing relationships when editing form observations - XFRM-31

--- api/src/main/java/org/openmrs/module/xforms/RelationshipSubmission.java
+++ api/src/main/java/org/openmrs/module/xforms/RelationshipSubmission.java
@@ -13,9 +13,20 @@
  */
 package org.openmrs.module.xforms;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.kxml2.kdom.Element;
 import org.openmrs.Patient;
 import org.openmrs.Person;
+import org.openmrs.Relationship;
+import org.openmrs.RelationshipType;
+import org.openmrs.api.APIException;
+import org.openmrs.api.PersonService;
+import org.openmrs.api.context.Context;
 
 /**
  * Utility for building relationship submission in from data entry xforms.
@@ -24,14 +35,105 @@ import org.openmrs.Person;
  */
 public class RelationshipSubmission {
 	
+	private static final Log log = LogFactory.getLog(RelationshipSubmission.class);
+	
 	/**
 	 * Saves relationships which have been edited, added, or deleted.
 	 * 
 	 * @param node the form root node.
 	 * @param patient the patient that the form has been submitted for.
+	 * @should create relationships
+	 * @should edit an editing relationship
 	 */
-	public static void submit(Element node, Patient patient) {
-	
-		//Person person; person.
+	public static void submit(Element rootElement, Patient patient) {
+		if (rootElement == null)
+			return;
+		
+		Element formNode = XformBuilder.getElement(rootElement, XformBuilder.NODE_FORM);
+		if (formNode == null)
+			return;
+		
+		PersonService ps = Context.getPersonService();
+		List<Relationship> relationships = new ArrayList<Relationship>();
+		for (int i = 0; i < formNode.getChildCount(); i++) {
+			if (formNode.getType(i) != Element.ELEMENT
+			        || !RelationshipBuilder.NODE_PATIENT_RELATIONSHIP.equals(formNode.getElement(i).getName())) {
+				continue;
+			}
+			//System.out.println("Found element..");
+			Element relationshipNode = formNode.getElement(i);
+			String uuid = relationshipNode.getAttributeValue(null, XformBuilder.ATTRIBUTE_UUID);
+			Person otherPerson = null;
+			RelationshipType relationshipType = null;
+			boolean isPersonA = false;
+			Person personA;
+			Person personB;
+			
+			//resolve the relationship type
+			Element relationshipTypeNode = XformBuilder.getElement(relationshipNode,
+			    RelationshipBuilder.BIND_PATIENT_RELATIONSHIP_TYPE_ID);
+			if (relationshipTypeNode != null) {
+				String relationTypeIdString = XformBuilder.getTextValue(relationshipTypeNode);
+				if (StringUtils.isNotBlank(relationTypeIdString))
+					relationshipType = ps.getRelationshipType(Integer.valueOf(relationTypeIdString));
+				if (relationshipType == null)
+					throw new APIException("Cannot find relation type with id:" + relationTypeIdString);
+			}
+			
+			//determine is this patient is A or B in the relationship
+			Element AorBNode = XformBuilder.getElement(relationshipNode,
+			    RelationshipBuilder.BIND_PATIENT_RELATIONSHIP_A_OR_B);
+			if (AorBNode == null) {
+				throw new APIException("Cannot determine if the patient is A or B in the relationship:"
+				        + relationshipType.getName());
+			}
+			String AorB = XformBuilder.getTextValue(AorBNode);
+			if (AorB != null && ("A".equalsIgnoreCase(AorB) || "B".equalsIgnoreCase(AorB))) {
+				isPersonA = "A".equalsIgnoreCase(AorB);
+			} else {
+				throw new APIException("Cannot determine if the patient is A or B in the relationship:"
+				        + relationshipType.getName());
+			}
+			
+			//get the relative
+			Element relativeNode = XformBuilder.getElement(relationshipNode, RelationshipBuilder.BIND_RELATIVE);
+			if (relativeNode != null) {
+				Element relativeUuidNode = XformBuilder.getElement(relativeNode, RelationshipBuilder.BIND_RELATIVE_UUID);
+				if (relativeUuidNode != null) {
+					String relativeUuid = XformBuilder.getTextValue(relativeUuidNode);
+					if (StringUtils.isNotBlank(relativeUuid))
+						otherPerson = ps.getPersonByUuid(relativeUuid);
+				}
+			}
+			if (otherPerson == null)
+				throw new APIException("Cannot determine if the relattive in the relationship:" + relationshipType.getName());
+			
+			if (isPersonA) {
+				personA = patient;
+				personB = otherPerson;
+			} else {
+				personB = patient;
+				personA = otherPerson;
+			}
+			
+			Relationship relationship = null;
+			if (StringUtils.isNotBlank(uuid)) {
+				relationship = ps.getRelationshipByUuid(uuid);
+				if (relationship == null)
+					throw new APIException("Failed to find relationship with uuid:" + uuid);
+			} else {
+				relationship = new Relationship();
+				relationship.setRelationshipType(relationshipType);
+			}
+			
+			relationship.setPersonA(personA);
+			relationship.setPersonB(personB);
+			
+			relationships.add(relationship);
+		}
+		
+		for (Relationship relationship : relationships) {
+			ps.saveRelationship(relationship);
+		}
 	}
 }
