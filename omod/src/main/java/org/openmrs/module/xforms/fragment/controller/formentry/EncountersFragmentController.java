e4dd8e71e5afe3c732c5c7a918f7fb1d23519429
dkayiwa <dkayiwa@openmrs.org>
2014-08-21 01:57:50 +0300

    Displaying an encounter list for editing

--- omod/src/main/java/org/openmrs/module/xforms/fragment/controller/formentry/EncountersFragmentController.java
+++ omod/src/main/java/org/openmrs/module/xforms/fragment/controller/formentry/EncountersFragmentController.java
@@ -1,6 +1,18 @@
 package org.openmrs.module.xforms.fragment.controller.formentry;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openmrs.Encounter;
+import org.openmrs.Form;
 import org.openmrs.Patient;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.Extension;
+import org.openmrs.module.ModuleFactory;
+import org.openmrs.module.Extension.MEDIA_TYPE;
+import org.openmrs.module.web.extension.FormEntryHandler;
 import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.fragment.FragmentModel;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,5 +22,46 @@ public class EncountersFragmentController {
 	
 	public void controller(@RequestParam("patientId") Patient patient, UiUtils ui,
 	                       FragmentModel model) {
+		
+		List<Encounter> encounters = Context.getEncounterService().getEncountersByPatient(patient);
+		model.put("encounters", encounters);
+		
+		Map<Form, String> viewUrlMap = new HashMap<Form, String>();
+		Map<Form, String> editUrlMap = new HashMap<Form, String>();
+		
+		List<Extension> handlers = ModuleFactory.getExtensions("org.openmrs.module.web.extension.FormEntryHandler",
+		    MEDIA_TYPE.html);
+		if (handlers != null) {
+			for (Extension ext : handlers) {
+				FormEntryHandler handler = (FormEntryHandler) ext;
+				{ // view
+					Collection<Form> toView = handler.getFormsModuleCanView();
+					if (toView != null) {
+						if (handler.getViewFormUrl() == null) {
+							throw new IllegalArgumentException("form entry handler " + handler.getClass()
+							        + " is trying to handle viewing forms but specifies no URL");
+						}
+						for (Form form : toView) {
+							viewUrlMap.put(form, handler.getViewFormUrl());
+						}
+					}
+				}
+				{ // edit
+					Collection<Form> toEdit = handler.getFormsModuleCanEdit();
+					if (toEdit != null) {
+						if (handler.getEditFormUrl() == null) {
+							throw new IllegalArgumentException("form entry handler " + handler.getClass()
+							        + " is trying to handle editing forms but specifies no URL");
+						}
+						for (Form form : toEdit) {
+							editUrlMap.put(form, handler.getEditFormUrl());
+						}
+					}
+				}
+			}
+		}
+		
+		model.put("formToViewUrlMap", viewUrlMap);
+		model.put("formToEditUrlMap", editUrlMap);
 	}
 }
---
+++ omod/src/main/webapp/fragments/formentry/encounters.gsp
@@ -0,0 +1,61 @@
+<%
+    ui.includeCss("uicommons", "datatables/dataTables.css")
+    ui.includeJavascript("uicommons", "datatables/jquery.dataTables.min.js")
+%>
+
+<script type="text/javascript">
+
+	<% if (encounters.size() > 0) { %>
+		jq(document).ready(function() {
+			var oTable = jq("#encounterTableParent > #encounterTable").dataTable({
+				"bPaginate": false,
+				"bAutoWidth": false,
+				"aaSorting": [[0, 'asc']]
+			});
+		});
+	<% } %>
+	
+</script>
+
+<div>
+
+    <div id="encounterTableParent">
+
+        <table id="encounterTable" width="100%" border="1" cellspacing="0" cellpadding="2">
+            <thead>
+                <tr>
+                    <th>${ ui.message("Encounter.form") }</th>
+                    <th>${ ui.message("ActiveLists.date") }</th>
+                    <th>${ ui.message("general.type") }</th>
+                    <th>${ ui.message("Encounter.provider") }</th>
+                    <th>${ ui.message("Encounter.location") }</th>
+                    <th>${ ui.message("Encounter.enterer") }</th>
+                </tr>
+            </thead>
+            <tbody>
+				<% if (encounters.size() == 0) { %>
+                    <tr>
+                        <td colspan="3">${ ui.message("emr.none") }</td>
+                    </tr>
+                <% } %>
+                
+                <% encounters.each { encounter -> %>
+                    <tr>
+                        <td> <a href=/${ ui.contextPath() }/${formToEditUrlMap[encounter.form]}?encounterId=${encounter.encounterId}&patientId=${patient.patientId}&refappui=true>${encounter.form.name}</a> </td>
+                        <td> ${ ui.formatDatetimePretty(encounter.encounterDatetime) }</td>
+                        <td> ${encounter.encounterType.name} </td>
+                        <td> 
+                        	<% encounter.providersByRoles.each { key, value -> %>
+			                	${value.name}
+			                <% } %>	 
+                        </td>
+                        <td> ${encounter.location} </td>
+                        <td> ${encounter.creator.personName} </td>
+                    </tr>
+                <% } %>
+                
+            </tbody>
+        </table>
+    </div>
+    
+</div>
