ba20e075865801983e8aeb7629df9499d81d2fd6
dkayiwa <kayiwadaniel@gmail.com>
2017-03-27 23:39:02 +0300

    Avoiding the addition of encounter types for non defaultEncounterTemplate extensions: https://talk.openmrs.org/t/visit-documents-ui-module-vdui-v1-1-throws-nullpointerexception-also-failed-to-attach-a-document-to-a-finished-visit/10236

--- api/src/main/java/org/openmrs/module/xforms/aop/AppFrameworkServiceAdvisor.java
+++ api/src/main/java/org/openmrs/module/xforms/aop/AppFrameworkServiceAdvisor.java
@@ -12,6 +12,7 @@ import org.openmrs.module.appframework.domain.Extension;
 
 public class AppFrameworkServiceAdvisor implements MethodInterceptor {
 	
+	@Override
 	public Object invoke(MethodInvocation invocation) throws Throwable {
 		
 		Object object = invocation.proceed();;
@@ -26,6 +27,10 @@ public class AppFrameworkServiceAdvisor implements MethodInterceptor {
 				for (Extension extension : extensions) {
 					Map<String, Object> extensionParams = extension.getExtensionParams();
 					Map<String, Object> types = (Map<String, Object>)extensionParams.get("supportedEncounterTypes");
+					
+					if (!"defaultEncounterTemplate".equals(extensionParams.get("templateId"))) {
+						continue;
+					}
 					
 					for (EncounterType type : encounterTypes) {
 						if ("ca3aed11-1aa4-42a1-b85c-8332fc8001fc".equals(type.getUuid()) || "25a042b2-60bc-4940-a909-debd098b7d82".equals(type.getUuid())) {
